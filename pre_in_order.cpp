ff80a1478b698d059b3bc2514bafd76c8c10def4
PBS <sortsimilar@gmail.com>
2018-06-28 23:34:07 +0800

    get post order from pre order and in order

--- pre_in_order.cpp
+++ pre_in_order.cpp
@@ -5,43 +5,41 @@ using namespace std;
 
 struct Node
 {
-	int left;
-	int right;
-	int name;
+	int key;
+	Node* left;
+	Node* right;	
 };
 
 
 vector<int> pre_order;
 vector<int> in_order;
 vector<int> post_order;
-vector<Node> binary;
 
-int build_tree(int inStart, int inEnd, int preStart, int preEnd)
+Node* build_tree(int inStart, int inEnd, int preStart, int preEnd)
 {
 	if (preStart > preEnd || inStart > inEnd)
 	{
-		return -1;
+		return NULL;
 	}
 
-	int root = pre_order[preStart];
+
+	Node* root = new Node;  
 
 	int k = 0;
 	for (int i = 0; i < in_order.size(); i++) 
 	{
-		if (in_order[i] == root) 
+		if (in_order[i] == pre_order[preStart]) 
 		{
 			k = i;
 			break;
 		}
 	}
 
+	root->key = pre_order[preStart];
+	root->left = build_tree(inStart, k-1, preStart+1, preStart+k-inStart);
+	root->right = build_tree(k+1, inEnd, preStart+(k-inStart)+1, preEnd);
 
-	binary[root].left = build_tree(inStart, k-1, preStart+1, preStart+k-inStart);
-	// Becuase k is not the length, it it need to -(inStart+1) to get the length
-	binary[root].right = build_tree(k+1, inEnd, preStart+(k-inStart)+1, preEnd);
-	// postStart+k-inStart = postStart+k-(inStart+1) +1
-
-	post_order.push_back(root+1);
+	post_order.push_back(root->key);
 
 	return root;
 }
@@ -59,40 +57,19 @@ int main()
 	{
 		int temp;
 		cin>>temp;
-		pre_order.push_back(temp-1);
+		pre_order.push_back(temp);
 	}
 
 	for(int i=0;i<N;i++)
 	{
 		int temp;
 		cin>>temp;
-		in_order.push_back(temp-1);
+		in_order.push_back(temp);
 	}
-
-// initialize binary tree;
-	for(int i=0;i<N;i++)
-	{
-		Node temp;
-		temp.name = i;
-		binary.push_back(temp);
-	}
-
-/*
-	for(int i=0;i<N;i++)    cout<<pre_order[i]<<" ";
-	cout<<endl;
-	for(int i=0;i<N;i++)    cout<<in_order[i]<<" ";
-	cout<<endl;
-*/
 
 
 	build_tree(0, N-1, 0, N-1);
 
-/*
-	for(int i=0;i<N;i++)
-	{
-		cout<<binary[i].name<<" "<<binary[i].left<<" "<<binary[i].right<<endl;
-	}
-*/
 
 	for(int i=0;i<N;i++)
 	{
@@ -108,10 +85,4 @@ int main()
 4 1 3 2 6 5 7
 1 2 3 4 5 6 7
 */
-
-
-
-
-
-
 

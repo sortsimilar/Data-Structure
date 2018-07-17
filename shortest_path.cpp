abeafe4aa90feb8386c3650b742530246f071f92
PBS <sortsimilar@gmail.com>
2018-07-17 20:26:19 +0800

    shortest path

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -5,8 +5,6 @@
 #include<vector>
 using namespace std;
 // Number of vertices in the graph
-#define num_v 9
-  
 
 struct Edge
 {
@@ -15,29 +13,18 @@ struct Edge
 	int weight;
 };
 
+int num_v;  
 vector<Edge> store_edge;
+vector<int> dist;     // The output array.  dist[i] will hold the shortest distance from src to i
+vector<bool> sptSet; // sptSet[i] will true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
+vector<int> adjacency_matrix;
 
-vector<int> dist;     // The output array.  dist[i] will hold the shortest
-                      // distance from src to i
+int get_weight(int start, int end)
+{
+	return adjacency_matrix[start*num_v + end];
+}
 
-vector<bool> sptSet; // sptSet[i] will true if vertex i is included in shortest
-                     // path tree or shortest distance from src to i is finalized
-
-vector<bool> adjacency_matrix;
-
-int graph[num_v][num_v]= {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-                      {4, 0, 8, 0, 0, 0, 0, 11, 0},
-                      {0, 8, 0, 7, 0, 4, 0, 0, 2},
-                      {0, 0, 7, 0, 9, 14, 0, 0, 0},
-                      {0, 0, 0, 9, 0, 10, 0, 0, 0},
-                      {0, 0, 4, 14, 10, 0, 2, 0, 0},
-                      {0, 0, 0, 0, 0, 2, 0, 1, 6},
-                      {8, 11, 0, 0, 0, 0, 1, 0, 7},
-                      {0, 0, 2, 0, 0, 0, 6, 7, 0}
-                     };
-
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
+// A utility function to find the vertex with minimum distance value, from the set of vertices not yet included in shortest path tree
 int minDistance()
 {
 	// Initialize min value
@@ -84,22 +71,25 @@ void dijkstra(int src)
 	// Find shortest path for all vertices
 	for (int count = 0; count < num_v-1; count++)
 	{
-	   // Pick the minimum distance vertex from the set of vertices not
-	   // yet processed. u is always equal to src in the first iteration.
-	   int u = minDistance();
+		// Pick the minimum distance vertex from the set of vertices not
+		// yet processed. u is always equal to src in the first iteration.
+		int u = minDistance();
 
-	   // Mark the picked vertex as processed
-	   sptSet[u] = true;
+		// Mark the picked vertex as processed
+		sptSet[u] = true;
 
-	   // Update dist value of the adjacent vertices of the picked vertex.
-	   for (int v = 0; v < num_v; v++)
+		// Update dist value of the adjacent vertices of the picked vertex.
+		for (int v = 0; v < num_v; v++)
+		{
+			// Update dist[v] only if is not in sptSet, there is an edge from 
+			// u to v, and total weight of path from src to  v through u is 
+			// smaller than current value of dist[v]
 
-		 // Update dist[v] only if is not in sptSet, there is an edge from 
-		 // u to v, and total weight of path from src to  v through u is 
-		 // smaller than current value of dist[v]
-		 if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-		                               && dist[u]+graph[u][v] < dist[v])
-		    dist[v] = dist[u] + graph[u][v];
+			if (!sptSet[v] && get_weight(u, v) && dist[u] != INT_MAX && dist[u]+get_weight(u, v) < dist[v])
+			{
+				dist[v] = dist[u] + get_weight(u, v);
+			}
+		}
 	}
 
 	// print the constructed distance array
@@ -109,6 +99,8 @@ void dijkstra(int src)
 // driver program to test above function
 int main()
 {
+	num_v = 9;
+
 	dist.resize(num_v);
 	sptSet.resize(num_v);
 
@@ -128,6 +120,22 @@ int main()
 		cin>>store_edge[i].weight;
 	}
 
+	// create adjacency matrix;
+	adjacency_matrix.resize(num_v*num_v);
+	for(int i=0;i<num_v*num_v;i++)
+	{
+		adjacency_matrix[i] = 0;
+	}
+
+	// save current amp in adjacency matrix;
+	for(int i=0;i<num_e;i++)
+	{
+		int start = store_edge[i].start;
+		int end = store_edge[i].end;
+
+		adjacency_matrix[start*num_v + end] = store_edge[i].weight;
+		adjacency_matrix[start + end*num_v] = store_edge[i].weight;
+	}
   
     dijkstra(0);
   

6516af6ccc207be1b2b7444ecd1c49d486a7314f
PBS <sortsimilar@gmail.com>
2018-07-31 22:51:59 +0800

    longest increasing subsequence

---
+++ long_increase.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+
+int CeilIndex(vector<int> &v, int l, int r, int key) 
+{
+    while (r-l > 1) 
+	{
+		int m = l + (r-l)/2;
+		if (v[m] >= key)    r = m;
+		else    l = m;
+    }
+ 
+    return r;
+}
+ 
+int LongestIncreasingSubsequenceLength(vector<int> &v) 
+{
+    if (v.size() == 0)    return 0;
+ 
+    vector<int> tail(v.size(), 0);
+    int length = 1; // always points empty slot in tail
+ 
+    tail[0] = v[0];
+    for (size_t i = 1; i < v.size(); i++) 
+	{
+        if (v[i] < tail[0])    tail[0] = v[i];
+        else if (v[i] > tail[length-1])    tail[length++] = v[i];
+        else    tail[CeilIndex(tail, -1, length-1, v[i])] = v[i];
+    }
+ 
+    return length;
+}
+ 
+
+
+int main() 
+{
+	vector<int> v(9);
+	v[0]=2;v[1]=5;v[2]=3;v[3]=7;v[4]=11;v[5]=8;v[6]=10;v[7]=13;v[8]=6;
+
+	cout<<"Length of Longest Increasing Subsequence is "<<LongestIncreasingSubsequenceLength(v);
+
+	return 0;
+}
+
+

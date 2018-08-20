28ae7345ee2bf034a246fe6a659e194d25d9199e
PBS <sortsimilar@gmail.com>
2018-08-20 20:21:08 +0800

    shortest path

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -1,5 +1,3 @@
-// A C++ program for Dijkstra's single source shortest path algorithm.
-// The program is for adjacency matrix representation of the graph
 #include<iostream>  
 #include<climits>
 #include<vector>
@@ -13,7 +11,6 @@ struct Edge
 };
 
 int num_v; // Number of vertices in the graph
-vector<Edge> store_edge;
 vector<int> dist;     // The output array.  dist[i] will hold the shortest distance from src to i
 vector<bool> sptSet; // sptSet[i] will true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
 vector<int> previous;
@@ -24,7 +21,7 @@ int get_weight(int start, int end)
 	return adjacency_matrix[start*num_v + end];
 }
 
-// A utility function to find the vertex with minimum distance value, from the set of vertices not yet included in shortest path tree
+
 int minDistance()
 {
 	// Initialize min value
@@ -61,12 +58,9 @@ int printSolution(int src)
 	}
 }
   
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
+
 void dijkstra(int src)
-{          
-  
-	// Initialize all distances as INFINITE and stpSet[] as false
+{            
 	for (int i=0;i<num_v;i++)
 	{
         dist[i] = INT_MAX;
@@ -74,26 +68,17 @@ void dijkstra(int src)
 		previous[i] = -1;
 	}
   
-	// Distance of source vertex from itself is always 0
+
 	dist[src] = 0;
   
-	// Find shortest path for all vertices
 	for (int i=0;i<num_v;i++)
 	{
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in the first iteration.
 		int u = minDistance();
-
-		// Mark the picked vertex as processed
 		sptSet[u] = true;
 
-		// Update dist value of the adjacent vertices of the picked vertex.
+
 		for (int v=0;v<num_v;v++)
 		{
-			// Update dist[v] only if is not in sptSet, there is an edge from 
-			// u to v, and total weight of path from src to  v through u is 
-			// smaller than current value of dist[v]
-
 			if (!sptSet[v] && get_weight(u, v) && dist[u]!=INT_MAX && dist[u]+get_weight(u, v)<dist[v])
 			{
 				dist[v] = dist[u] + get_weight(u, v);
@@ -104,7 +89,7 @@ void dijkstra(int src)
 
 }
   
-// driver program to test above function
+
 int main()
 {
 	num_v = 9;
@@ -113,16 +98,7 @@ int main()
 	sptSet.resize(num_v);
 	previous.resize(num_v);
 
-	int num_e = 14;
-	store_edge.resize(num_e);
-	for(int i=0;i<num_e;i++)
-	{
-		cin>>store_edge[i].start;
-		cin>>store_edge[i].end;
-		cin>>store_edge[i].weight;
-	}
 
-	// create adjacency matrix;
 	adjacency_matrix.resize(num_v*num_v);
 	for(int i=0;i<num_v*num_v;i++)
 	{
@@ -130,13 +106,18 @@ int main()
 	}
 
 	// save current amp in adjacency matrix;
+	int num_e = 14;
 	for(int i=0;i<num_e;i++)
 	{
-		int start = store_edge[i].start;
-		int end = store_edge[i].end;
+		int start;
+		cin>>start;
+		int end;
+		cin>>end;
+		int weight;
+		cin>>weight;
 
-		adjacency_matrix[start*num_v + end] = store_edge[i].weight;
-		adjacency_matrix[start + end*num_v] = store_edge[i].weight;
+		adjacency_matrix[start*num_v + end] = weight;
+		adjacency_matrix[start + end*num_v] = weight;
 	}
   
     dijkstra(1);

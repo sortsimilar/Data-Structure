debe40c3148d7a666bf355b78e94671e457dec21
PBS <sortsimilar@gmail.com>
2018-09-03 12:11:56 +0800

    merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -6,20 +6,22 @@ using namespace std;
 
 vector<int> arr;
 
-
-
-int min(int x, int y) { return (x<y)? x :y; }
+int get_min(int x, int y) { return (x<y)? x :y; }
  
-
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
 void merge(int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
  
-    int L[n1];
+	/* create temp arrays */
+	int L[n1];
 	int R[n2];
  
+	/* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)	L[i] = arr[l + i];
     for (j = 0; j < n2; j++)	R[j] = arr[m + 1+ j];
  
@@ -69,11 +71,25 @@ void merge_sort()
        {
            int mid = left_start + curr_size - 1;
  
-           int right_end = min(left_start + 2*curr_size - 1, arr.size()-1);
+           int right_end = get_min(left_start + 2*curr_size - 1, arr.size()-1);
  
            merge(left_start, mid, right_end);
        }
    }
+}
+
+
+void merge_one_more()
+{ 
+	int curr_size = 2;
+	for (int left_start=0; left_start<arr.size()-1; left_start += 2*curr_size)
+	{
+	   int mid = left_start + curr_size - 1;
+
+	   int right_end = get_min(left_start + 2*curr_size - 1, arr.size()-1);
+
+	   merge(left_start, mid, right_end);
+	}
 }
 
 
@@ -94,18 +110,24 @@ void printArray()
 
 int main()
 { 
-	arr.resize(6);
-	arr[0] = 12;
-	arr[1] = 11;
-	arr[2] = 13;
-	arr[3] = 5;
-	arr[4] = 6;
+	arr.resize(10);
+	arr[0] = 1;
+	arr[1] = 3;
+	arr[2] = 2;
+	arr[3] = 8;
+	arr[4] = 5;
 	arr[5] = 7;
+	arr[6] = 4;
+	arr[7] = 9;
+	arr[8] = 0;
+	arr[9] = 6;
 
     cout<<"Given array is "<<endl;
     printArray();
  
-    merge_sort();
+//    merge_sort();
+
+	merge_one_more();
  
     cout<<"Sorted array is "<<endl;
     printArray();
@@ -113,6 +135,8 @@ int main()
     return 0;
 }
 
-
-
-
+/*
+10
+3 1 2 8 7 5 9 4 0 6
+1 3 2 8 5 7 4 9 0 6
+*/
